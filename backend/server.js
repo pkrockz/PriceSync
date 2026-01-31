9ec1758b0a02957c4e84aa595e45765de69d33b0
Pratham Prashant Kutty <prathamkutty@gmail.com>
2026-01-31 16:58:05 +0530

    Fixed port binding for railway

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.use("/products", productRoutes);
 const orderRoutes = require("./routes/orders");
 app.use("/orders", orderRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port", process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
 });
